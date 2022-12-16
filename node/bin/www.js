ad17380ec635bbbc55bd7f447d4251997c55c912
dyllan-Lee <dltjrwls0802@gmail.com>
2022-12-16 17:47:38 +0900

    port change

--- node/bin/www.js
+++ node/bin/www.js
@@ -1,7 +1,9 @@
 let app = require('../app');
+let port = 3000;
+// let port = process.env.PORT || 3000;
 
-let port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`${port} 로  express 실행`);
+// })
 
-app.listen(port, () => {
-  console.log(`${port} 로  express 실행`);
-})
+app.listen(process.env.PORT || port)
